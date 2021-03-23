42038fb860cb62943f98980d28cee14fb0437988
Maxime Lardeur <maxime@fruitionsciences.com>
2021-03-23 11:12:19 +0100

    Last commit before migrating to EndeavourOS

--- scripts/home-backup.sh
+++ scripts/home-backup.sh
@@ -1,5 +1,5 @@
 sudo rsync -aAXHP --delete \
   --info=progress2 \
   -e "ssh -i /home/maxime/.ssh/id_rsa" \
-  --exclude={"/home/*/.thumbnails/*","/home/*/.cache/mozilla/*","/home/*/.cache/chromium/*","/home/*/.local/share/Trash/*"} \
+  --exclude={"/home/*/.thumbnails/*","/home/*/.cache/mozilla/*","/home/*/.cache/spotify/*","/home/*/.cache/chromium/*","/home/*/.local/share/Trash/*"} \
   /home/ root@192.168.1.4:/mnt/media/backup/home/
