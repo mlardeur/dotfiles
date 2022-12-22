338cd9062bf62b8fa35fb35fce8a4352a9667d15
Maxime Lardeur <maxime@fruitionsciences.com>
2022-12-22 19:09:17 +0100

    Add flavours to manage colors schemes

--- .config/sway/scripts/blur-lock.sh
+++ .config/sway/scripts/blur-lock.sh
@@ -1,9 +1,20 @@
+#!/usr/bin/env bash
+
 #!/bin/sh
 # Times the screen off and puts it to background
 swayidle \
     timeout  300 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' &
-# Locks the screen immediately
-swaylock
+
+# Take a screenshot and apply blur to it
+PICTURE=/tmp/swaylock.png
+BLUR="5x4"
+grim $PICTURE
+convert $PICTURE -blur $BLUR $PICTURE
+
+# lock the screen with blured picture
+swaylock -i $PICTURE
+rm $PICTURE
+
 # Kills last background task so idle timer doesn't keep running
 kill %%
